29380768e41e60e1da27aa22cd6d79a15fa617c6
aaronk6 <aaronk6@users.noreply.github.com>
2015-07-19 14:50:57 +0200

    Giphy Trending: Added option to configure desired format (gif, mp4, webp)

--- widgets/giphy_trending.rb
+++ widgets/giphy_trending.rb
@@ -3,15 +3,23 @@ require_relative '../classes/pull_widget'
 class GiphyTrending < PullWidget
 
   DEFAULT_API_KEY = 'dc6zaTOxFJmzC' # Giphy test key
+  FORMAT_GIF = 'gif'
+  FORMAT_MP4 = 'mp4'
+  FORMAT_WEBP = 'webp'
+  DEFAULT_FORMAT = FORMAT_GIF
+  SUPPORTED_FORMATS = [ FORMAT_GIF, FORMAT_MP4, FORMAT_WEBP ]
   SUPPORTED_ITEM_TYPE = 'gif'
-  MAX_RESULTS = 100 # 100 per request is Giphy's maximum
+  MAX_RESULTS = 100 # 100 is Giphy's maximum (per request)
 
   def initialize(*args)
     super
 
     @endpoint = 'https://api.giphy.com/v1'
-    @update_interval = 300
+    @update_interval = 900
     @config["api_key"] = DEFAULT_API_KEY if not @config["api_key"]
+    if not @config["format"] or not SUPPORTED_FORMATS.include? @config["format"]
+      @config["format"] = DEFAULT_FORMAT
+    end
   end
 
   private
@@ -21,16 +29,26 @@ class GiphyTrending < PullWidget
     data = JSON.load(res)["data"]
 
     items = []
-    data.each do |item|
-      next if item["type"] != SUPPORTED_ITEM_TYPE
-      items.push({
-        id: item["id"],
-        url: item["images"]["fixed_height"]["url"]
-      })
+    data.each do |i|
+      next if i["type"] != SUPPORTED_ITEM_TYPE
+      item = {
+        id: i["id"]
+      }
+      case @config["format"]
+      when FORMAT_GIF
+        item[:url] = i["images"]["fixed_height"]["url"]
+      when FORMAT_MP4
+        item[:url] = i["images"]["fixed_height"]["mp4"]
+      when FORMAT_WEBP
+        item[:url] = i["images"]["fixed_height"]["webp"]
+      end
+      next if not item[:url] or item[:url] == ""
+      items.push item
     end
 
     {
-      items: items
+      format: @config["format"],
+      items: items || []
     }
   end
 
